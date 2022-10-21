dc165d029a5ccf9145c66e8c926cf929133d20e4
David Rios <david.r.v@hotmail.com>
2022-10-21 00:43:35 -0300

    update page empresa

--- Pages/script.js
+++ Pages/script.js
@@ -26,3 +26,13 @@ function changeSlide(direction) {
 
 upButton.addEventListener("click", () => changeSlide("up"));
 downButton.addEventListener("click", () => changeSlide("down"));
+
+window.onscroll = function (){
+    var posicion = window.pageYOffset || document.documentElement.scrollTop;
+    
+    var elemento1 = document.getElementById("icon_fork");
+    var elemento2 = document.getElementById("icon_fire");
+
+    elemento1.style.bottom = posicion * 0.1 + "px";
+    elemento2.style.top = posicion * 0.2 + "px";
+}

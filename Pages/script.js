d7e4714848fb0122c993fec87f8c2c784b214e8d
David Rios <david.r.v@hotmail.com>
2022-10-24 00:10:39 -0300

    nuevo media servicios

--- Pages/script.js
+++ Pages/script.js
@@ -9,23 +9,33 @@ let activeSlideIndex = 0;
 
 slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
 
-function changeSlide(direction) {
-    const sliderHeight = sliderContainer.clientHeight;
-    if (direction === "up") {
-        activeSlideIndex++;
-        if (activeSlideIndex > slidesLength - 1)
-            activeSlideIndex = 0;
-    } else if (direction === "down") {
-        activeSlideIndex--;
-        if (activeSlideIndex < 0)
-            activeSlideIndex = slidesLength - 1;
-    }
-    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`;
-    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`;
-}
-
 upButton.addEventListener("click", () => changeSlide("up"));
 downButton.addEventListener("click", () => changeSlide("down"));
+
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight;
+  if (direction === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex > slidesLength - 1) {
+      activeSlideIndex = 0;
+    }
+  } else if (direction === "down") {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesLength - 1;
+    }
+  }
+
+  slideRight.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }px)`;
+  slideLeft.style.transform = `translateY(${
+    activeSlideIndex * sliderHeight
+  }px)`;
+};
+
+
+/* SCROLL*/
 
 window.onscroll = function (){
     var posicion = window.pageYOffset || document.documentElement.scrollTop;

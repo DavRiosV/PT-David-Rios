b546dfbc2ca6f033d567017043710d91f3478078
David Rios <david.r.v@hotmail.com>
2023-08-31 23:26:40 -0400

    tostify ok

--- Pages/index.js
+++ Pages/index.js
@@ -1,13 +1,16 @@
-const btnInfo = document.querySelector("button.cartaInfo")
+const btnsInfo = document.querySelectorAll("button.cartaInfo");
 
-btnInfo.addEventListener('click', masInfo);
-function masInfo () {
-	Swal.fire({
-		title: 'Sweet!',
-		text: 'Modal with a custom image.',
-		imageUrl: 'https://unsplash.it/400/200',
-		imageWidth: 400,
-		imageHeight: 200,
-		imageAlt: 'Custom image',
-	})
+btnsInfo.forEach(btn => {
+  btn.addEventListener('click', masInfo);
+});
+
+function masInfo() {
+  Swal.fire({
+    title: 'Sweet!',
+    text: 'Modal with a custom image.',
+    imageUrl: 'https://unsplash.it/400/200',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  });
 }

c3c5e92fd01ef29941a94387b5a83df8de2c2dfb
David Rios <david.r.v@hotmail.com>
2023-11-14 01:30:16 -0300

    modificaciones al home

--- Pages/index.js
+++ Pages/index.js
@@ -1,15 +1,16 @@
-document.getElementById('ton3').addEventListener('click', function(event) {
-  event.preventDefault();
+const btnsInfo = document.querySelectorAll("button.cartaInfo");
 
-  // Simulación de carga de la imagen del PDF (reemplazar con tu lógica de conversión)
-  const pdfImageURL = './PDF/5ton.pdf';
-
-  // Mostrar la imagen del PDF en la notificación
-  Toastify({
-      text: `<img src="${pdfImageURL}" alt="PDF">`, // Insertar la imagen aquí
-      duration: 0, // Duración indefinida o hasta que el usuario cierre la notificación
-      gravity: "top",
-      close: true,
-      escapeMarkup: false // Permite mostrar contenido HTML en la notificación
-  }).showToast();
+btnsInfo.forEach(btn => {
+  btn.addEventListener('click', masInfo);
 });
+
+function masInfo() {
+  Swal.fire({
+    title: 'Sweet!',
+    text: 'Modal with a custom image.',
+    imageUrl: 'https://unsplash.it/400/200',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  });
+}
